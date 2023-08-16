6b6e2856526a29f40458381676346c1b8e3ed37b
KevinV-AD <kvu0@our.ecu.edu.au>
2023-08-16 17:43:53 +0800

    my first iteration of workshop

---
+++ Mod4WorksShop.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+#Workshop 4 - Task 1 
+ puts "What's Your Name? "
+
+ #method to get name
+ name = gets.chomp
+
+ #method to get User input : Food 
+ puts "What's your Favourite Food? "
+ #method for getting favourite type of food from EU. 
+ food = gets.chomp 
+
+ puts "Hi " + name + " I hear you like " + food
+
+ 
